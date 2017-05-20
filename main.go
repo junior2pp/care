88db72c9ff3c6f43d63f820a1a474c6fedf92386
Roberto Suarez <electrosonix12@gmail.com>
2017-05-20 16:38:45 +0000

    agregamos la funcion noticia

--- main.go
+++ main.go
@@ -11,11 +11,23 @@ func main() {
 	http.Handle("/css/", fs)
 	http.Handle("/imgs/", fs)
 	http.HandleFunc("/", home)
+	http.HandleFunc("/", noticia)
 	http.ListenAndServe(":8080", nil)
 }
 
 func home(w http.ResponseWriter, r *http.Request)  {
 	t,err := template.ParseFiles("./public/html/home.html")
+	if err != nil{
+		log.Println(err)
+	}
+	err = t.Execute(w,nil)
+	if err != nil{
+		log.Println(err)
+	}
+}
+
+func noticia(w http.ResponseWriter, r *http.Request)  {
+	t,err := template.ParseFiles("./public/html/noticias.html")
 	if err != nil{
 		log.Println(err)
 	}
