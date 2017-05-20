187bf1ee45e17725c24707929acbbe810efe927a
Roberto Suarez <electrosonix12@gmail.com>
2017-05-20 15:50:24 +0000

    tenemos el servidor de archivo estaticos

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import "net/http"
 
+func main() {
+	fs := http.FileServer(http.Dir("./public"))
+	http.Handle("/css/", fs)
+	http.Handle("/html/", fs)
+	http.Handle("/imgs/", fs)
+	http.ListenAndServe(":8080", nil)
 }
