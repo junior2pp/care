97430d8f26f87c261668de96d3ff019bb29da17e
Roberto Suarez <electrosonix12@gmail.com>
2017-05-20 16:04:02 +0000

    metimos un handlefunc para la pantalla del home

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"text/template"
+	"log"
+)
 
 func main() {
 	fs := http.FileServer(http.Dir("./public"))
 	http.Handle("/css/", fs)
-	http.Handle("/html/", fs)
 	http.Handle("/imgs/", fs)
+	http.HandleFunc("/", home)
 	http.ListenAndServe(":8080", nil)
+}
+
+func home(w http.ResponseWriter, r *http.Request)  {
+	t,err := template.ParseFiles("./public/html/home.html")
+	if err != nil{
+		log.Println(err)
+	}
+	err = t.Execute(w,nil)
+	if err != nil{
+		log.Println(err)
+	}
 }
